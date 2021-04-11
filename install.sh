5fe7ccbc6e4887fee10b05a85af376d52b4191f8
williamzborja <rodriguezborjawilliam@gmail.com>
2021-04-11 10:17:42 -0500

    fix betty style

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+sudo apt update && sudo apt upgrade -y
 sudo apt install valgrind gcc emacs -y
 cp .emacs ~/.emacs
 cp .bashrc ~/.bashrc
 
-
 git clone https://github.com/holbertonschool/Betty.git
-sudo ./Betty/install.sh
-sudo rm  -r Betty
+cd ./Betty
+sudo ./install.sh
+cd ..
+sudo rm -r ./Betty
